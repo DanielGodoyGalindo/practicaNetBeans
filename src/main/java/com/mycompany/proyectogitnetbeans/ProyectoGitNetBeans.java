c525e0252c03451348b8b49c0f825650cf6b2372
Daniel Godoy <danielgodoy66@gmail.com>
2024-12-07 15:37:09 +0100

    modificado mensaje

---
+++ src/main/java/com/mycompany/proyectogitnetbeans/ProyectoGitNetBeans.java
@@ -0,0 +1,12 @@
+package com.mycompany.proyectogitnetbeans;
+
+/**
+ *
+ * @author Dany_
+ */
+public class ProyectoGitNetBeans {
+
+    public static void main(String[] args) {
+        System.out.println("Cambio el mensaje");
+    }
+}
